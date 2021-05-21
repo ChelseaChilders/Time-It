6fa7727ec87d972fddce27cfb63301f90b754e82
Chelsea Childers <chelsea.childers@centene.com>
2021-05-20 21:54:46 -0400

    Add tool-bar with icosns

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-const materialModules = [MatButtonModule];
+const materialModules = [MatButtonModule, MatToolbarModule, MatIconModule];
 
 @NgModule({
   imports: materialModules,
