a3cc75a9888dd47f5ede9750ba868b617454e71a
Chelsea Childers <chelsea.childers@centene.com>
2021-05-21 11:17:38 -0400

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAU30_EOe2rps_uM_2XYSf9oypozYZyUas',
+    authDomain: 'kanbanfire-ae4fb.firebaseapp.com',
+    projectId: 'kanbanfire-ae4fb',
+    storageBucket: 'kanbanfire-ae4fb.appspot.com',
+    messagingSenderId: '595834894325',
+    appId: '1:595834894325:web:0272c3a2c91787db2c52bc',
+    measurementId: 'G-MWLB5BE9G7',
+  },
 };
 
 /*
